318b582183cfc177b0b5d28adddce05b6d92ebd6
YuryKoropenko <yurykoropenko@gmail.com>
2017-07-20 17:05:13 +0300

    commit 3

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	$('.mob__h-nav button').on('click', function() {
+		$(this).parent().parent().children('.h-nav').stop(false, true).slideToggle();
+		$(document).click(function(event) {
+			if ($(event.target).closest('.h-nav').length) return;
+				$('.h-nav').slideUp();
+				event.stopPropagation();
+			});
+		return false;
+	});
+});
